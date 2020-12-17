617cfdfc629cf4030b03a11d2bd67dfc076ab788
Brittney <desertdev2613@gmail.com>
2020-12-17 14:02:06 -0700

    Created form view. Set get route to show home page and began post method. Post method not completed.

---
+++ id_card/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+// POST method
+router.post('/completedId', function (req, res) {
+
+
+
+
+})
+
+module.exports = router;
